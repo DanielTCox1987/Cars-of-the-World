ab3c28db65e28ffe7c030e1f47695443f45874ca
Daniel Cox <danieltcox1987@gmail.com>
2015-11-24 16:02:04 -0500

    Add some text explaining what I added.

--- app.js
+++ app.js
@@ -1,117 +1,121 @@
 /*global $, document */
 
-var CarFactory = (function () {
+var CarFactory = (function() {
+  /**
+   * Creates a car factory that can build new car objects
+   * @constructor Factory
+   */
+  var Factory = function() {};
+
+  Factory.prototype = {
     /**
-     * Creates a car factory that can build new car objects
-     * @constructor Factory
+     * Builds a car from the provided spec
+     * @param  {Object} specs Car details of the form
+     * {
+     * 		make: {String},
+     * 		model: {String},
+     * 		year: {Number}
+     * }
+     * @return {Car}          the built car
      */
-    var Factory = function () {};
+    build: function(specs) {
+      // validate that specs is an object
+      if (specs === null || typeof specs !== "object") {
+        throw new Error("CarFactory.setspecs specs parameter is not an object");
+      }
 
-    Factory.prototype = {
+      var car = {
         /**
-         * Builds a car from the provided spec
-         * @param  {Object} specs Car details of the form
-         * {
-         * 		make: {String},
-         * 		model: {String},
-         * 		year: {Number}
-         * }
-         * @return {Car}          the built car
+         * generates the html for a car list item
+         * @return {String} the html string
          */
-        build: function (specs) {
-            // validate that specs is an object
-            if (specs === null || typeof specs !== "object") {
-                throw new Error("CarFactory.setspecs specs parameter is not an object");
-            }
-
-            var car = {
-                /**
-                 * generates the html for a car list item
-                 * @return {String} the html string
-                 */
-                render: function () {
-                    var html = "<li>";
-                    html += this.make + " ";
-                    html += this.model + " ";
-                    html += this.year + " ";
-                    html += this.country + " ";
-                    html += "</li>";
-                    return html;
-                }
-            };
-
-            // validate the existance of specs.make and it's string type
-            if (specs.make && typeof specs.make === "string") {
-                car.make = specs.make;
-            }
-            if (specs.model && typeof specs.model === "string") {
-                car.model = specs.model;
-            }
-            if (specs.year && typeof specs.year === "number") {
-                car.year = specs.year;
-            }
-            if (specs.country && typeof specs.country === "string") {
-            car.country = specs.country;
-            }
-
-            return car;
-        },
-
-        /**
-         * Builds a separate car based on the spec for each year in
-         * the year range
-         * @param  {Object} specs     Car details
-         * @param  {Number} startYear The starting year
-         * @param  {Number} endYear   The ending year
-         * @return {Array}            an array of cars
-         */
-        buildAllYears: function (specs, startYear, endYear) {
-            var range = Array.apply(null, Array(endYear - startYear + 1))
-                .map(function (_, i) {
-                    return i + startYear;
-                });
-
-            return range.map(function (year) {
-                return this.build({
-                    year: year,
-                    make: specs.make,
-                    model: specs.model,
-                    country: specs.country
-                });
-            }.bind(this));
+        render: function() {
+          var html = "<li>";
+          html += this.make + " ";
+          html += this.model + " ";
+          html += this.year + " ";
+          html += this.country + " ";
+          html += "</li>";
+          return html;
         }
-    };
+      };
 
-    return Factory;
+      // validate the existance of specs.make and it's string type
+      if (specs.make && typeof specs.make === "string") {
+        car.make = specs.make;
+      }
+      if (specs.model && typeof specs.model === "string") {
+        car.model = specs.model;
+      }
+      if (specs.year && typeof specs.year === "number") {
+        car.year = specs.year;
+      }
+      if (specs.country && typeof specs.country === "string") {
+        car.country = specs.country;
+      }
+
+      return car;
+    },
+
+    /**
+     * Builds a separate car based on the spec for each year in
+     * the year range
+     * @param  {Object} specs     Car details
+     * @param  {Number} startYear The starting year
+     * @param  {Number} endYear   The ending year
+     * @return {Array}            an array of cars
+     */
+    buildAllYears: function(specs, startYear, endYear) {
+      var range = Array.apply(null, Array(endYear - startYear + 1))
+        .map(function(_, i) {
+          return i + startYear;
+        });
+
+      return range.map(function(year) {
+        return this.build({
+          year: year,
+          make: specs.make,
+          model: specs.model,
+          country: specs.country
+        });
+      }.bind(this));
+    }
+  };
+
+  return Factory;
 }()); //IIFE - immediately invoked function expression
 
-var CarManufacturer = (function () {
-    /**
-     * Creates an instance of the Manufacturer class
-     * @constructor
-     * @returns {Manufacturer} A car Manufacturer
-     */
-    var Manufacturer = function() {};
+var CarManufacturer = (function() {
+  /**
+   * Creates an instance of the Manufacturer class
+   * @constructor
+   * @returns {Manufacturer} A car Manufacturer
+   */
+  var Manufacturer = function() {};
 
-    Manufacturer.prototype = {
-        // need a factory... or two? just one is fine for now.
-        init: function(name) {
-            this.factory = new CarFactory();
-            this.name = name;
-            // return this;
-        },
+  Manufacturer.prototype = {
+    // need a factory... or two? just one is fine for now.
+    init: function(name) {
+      this.factory = new CarFactory();
+      this.name = name;
+      // return this;
+    },
 
-        // build models
-        build: function() {
+    // build models
+    build: function() {
 
-        }
-    };
+    }
+  };
 
-    return Manufacturer;
+  return Manufacturer;
 }());
 
-var CarModel = (function ()  {
-
+var CarModel = (function() {
+  /**
+   *Creates an instane of a Model class.
+   *@constructor
+   *@returns {Model} A car Model.
+   */
   var Model = function() {};
 
   Model.prototype = {
@@ -122,8 +126,8 @@ var CarModel = (function ()  {
 
     },
 
-    build:function() {
-      
+    build: function() {
+
     }
 
   }
@@ -131,58 +135,58 @@ var CarModel = (function ()  {
 })
 
 var cars = {
-    store: [],
+  store: [],
 
-    /**
-     * pushes a single car or an array of cars to the
-     * cars.store property
-     * @param {Car | Array} car the car(s)
-     */
-    add: function (car) {
-        if (car instanceof Array) {
-            car.forEach(function(car) {
-                this.add(car);
-            }.bind(this));
-            return;
-        }
-        this.store.push(car);
-    },
-
-    /**
-     * generates the html for all the cars in my store
-     * @return {String} the html string
-     */
-    render: function () {
-        var html = "<ul>";
-        html += this.store.map(function (car) {
-            return car.render();
-        }).join("");
-        html += "</ul>";
-        return html;
+  /**
+   * pushes a single car or an array of cars to the
+   * cars.store property
+   * @param {Car | Array} car the car(s)
+   */
+  add: function(car) {
+    if (car instanceof Array) {
+      car.forEach(function(car) {
+        this.add(car);
+      }.bind(this));
+      return;
     }
+    this.store.push(car);
+  },
+
+  /**
+   * generates the html for all the cars in my store
+   * @return {String} the html string
+   */
+  render: function() {
+    var html = "<ul>";
+    html += this.store.map(function(car) {
+      return car.render();
+    }).join("");
+    html += "</ul>";
+    return html;
+  }
 };
 
 /**
  * generates the html string for the page
  * @return {String} the html
  */
-var render = function () {
-    var html = "<h1>Cars of the World</h1>";
-    html += cars.render();
-    return html;
+var render = function() {
+  var html = "<h1>Cars of the World</h1>";
+  html += cars.render();
+  return html;
 };
 
 /**
  * Creates all the cars
  * @return {undefined} undefined
  */
-var makeCars = function () {
-    var factory = new CarFactory();
-    cars.add(factory.buildAllYears({
-        make: "Jeep",
-        model: "Cherokee",
-        country: "United States"
-    }, 1984, 2001));
+var makeCars = function() {
+  var factory = new CarFactory();
+  cars.add(factory.buildAllYears({
+    make: "Jeep",
+    model: "Cherokee",
+    country: "United States"
+  }, 1984, 2001));
 };
 
 /**
@@ -191,15 +195,15 @@ var makeCars = function () {
  * @param  {jQuery} $rootElement The root element of the page
  * @return {undefined}           undefined
  */
-var init = function ($rootElement) {
-    makeCars();
-    var htmlString = render();
-    $rootElement.append(htmlString);
+var init = function($rootElement) {
+  makeCars();
+  var htmlString = render();
+  $rootElement.append(htmlString);
 };
 
-$(document).ready(function () {
-    var $rootElement = $("body");
-    init($rootElement);
+$(document).ready(function() {
+  var $rootElement = $("body");
+  init($rootElement);
 });
 
 // displaying specific cars
